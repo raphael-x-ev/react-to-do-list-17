41286956681f12d5e3ddb860129934e79c91adb1
rap.ev <raphaelmoa12@gmail.com>
2021-08-06 06:42:22 +0800

    Update component hooks for list and notes

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,48 +1,49 @@
 import React from "react";
 
-export default class List extends React.Component {
-  render() {
-    return (
-      <div>
-        <table className="table table-striped border-bottom">
-          <thead>
-            <tr>
-              <th
-                className="todo-header"
-                scope="col"
-                onClick={() => this.props.onSortByToDoName()}
-              >
-                To Do
-              </th>
-              <th
-                className="todo-header"
-                scope="col"
-                onClick={() => this.props.onSortByToDoDate()}
-              >
-                Date Added
-              </th>
-              <th scope="col"></th>
+const List = (props) => {
+  return (
+    <div>
+      <table className="table table-striped border-bottom">
+        <thead>
+          <tr>
+            <th
+              className="todo-header"
+              scope="col"
+              // onClick={() => this.props.onSortByToDoName()}
+            >
+              To Do
+            </th>
+            <th
+              className="todo-header"
+              scope="col"
+              // onClick={() => this.props.onSortByToDoDate()}
+            >
+              Date Added
+            </th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.values(props.list).map((value, index) => (
+            <tr data-index={index} key={value.name}>
+              <td>{value.name}</td>
+              <td>{value.date}</td>
+              <td id="table-action">
+                <button
+                  data-index={index}
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={props.onRemoveToDo}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {Object.values(this.props.list).map((value, index) => (
-              <tr key={value.name}>
-                <td>{value.name}</td>
-                <td>{value.date}</td>
-                <td id="table-action">
-                  <button
-                    type="button"
-                    className="btn btn-danger"
-                    onClick={() => this.props.onRemoveToDo(value.name)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default List;
--- src/components/notes.jsx
+++ src/components/notes.jsx
@@ -1,72 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import List from "./list";
 
-export default class Notes extends React.Component {
-  //initial state
-  state = {
-    todoList: [
-      { name: "Clearn React", date: "8/3/2021, 7:19:01 PM" },
-      { name: "Eat Breakfast", date: "8/2/2021, 7:29:01 PM" },
-      { name: "Be a Rockstar Developer", date: "8/4/2021, 7:35:00 PM" },
-    ],
-    input: "",
-  };
-
-  //this bind
-  handleRemoveToDo = this.handleRemoveToDo.bind(this);
-  updateInput = this.updateInput.bind(this);
-  handleAddToDo = this.handleAddToDo.bind(this);
-  handleSortToDoName = this.handleSortToDoName.bind(this);
-  handleSortToDoDate = this.handleSortToDoDate.bind(this);
+const Notes = () => {
+  const [todo, setToDo] = useState([
+    { name: "Clearn React", date: "8/3/2021, 7:19:01 PM" },
+    { name: "Eat Breakfast", date: "8/2/2021, 7:29:01 PM" },
+    { name: "Be a Rockstar Developer", date: "8/4/2021, 7:35:00 PM" },
+  ]);
+  const [input, setInput] = useState("");
 
   //Methods
-  handleAddToDo() {
-    const obj = {
-      name: this.state.input,
-      date: new Date().toLocaleString() + "",
-    };
+
+  const handleAddToDo = () => {
     let nameListArray = [];
 
-    this.setState((previousState) => {
-      Object.values(previousState.todoList).forEach((value) => {
-        nameListArray.push(value.name);
-      });
-
-      if (!nameListArray.includes(this.state.input)) {
-        return {
-          todoList: previousState.todoList.concat(obj),
-          input: "",
-        };
-      }
-      //adds if the value is not yet inside the object
+    Object.values(todo).forEach((value) => {
+      nameListArray.push(value.name);
     });
-  }
 
-  handleRemoveToDo(name) {
+    if (!nameListArray.includes(input)) {
+      const newTodos = [
+        ...todo,
+        { name: input, date: new Date().toLocaleString() + "" },
+      ];
+      setToDo(newTodos);
+      setInput("");
+    }
+  };
+
+  const handleRemoveToDo = (e) => {
+    e.stopPropagation();
+    const index = e.currentTarget.getAttribute("data-index");
+    const newTodos = [...todo];
+    newTodos.splice(index, 1);
+    setToDo(newTodos);
+  };
+
+  function handleSortToDoName() {
     this.setState((previousState) => {
       return {
-        todoList: previousState.todoList.filter((todo) => todo.name !== name),
+        todoList: previousState.todoList.sort(dynamicSort("name")),
       };
     });
   }
 
-  handleSortToDoName() {
+  function handleSortToDoDate() {
     this.setState((previousState) => {
       return {
-        todoList: previousState.todoList.sort(this.dynamicSort("name")),
+        todoList: previousState.todoList.sort(dynamicSort("date")),
       };
     });
   }
 
-  handleSortToDoDate() {
-    this.setState((previousState) => {
-      return {
-        todoList: previousState.todoList.sort(this.dynamicSort("date")),
-      };
-    });
-  }
-
-  dynamicSort(property) {
+  function dynamicSort(property) {
     var sortOrder = 1;
     if (property[0] === "-") {
       sortOrder = -1;
@@ -80,37 +66,35 @@ export default class Notes extends React.Component {
   }
 
   //update inputs (event)
-  updateInput(typeEvent) {
+  const updateInput = (typeEvent) => {
     const value = typeEvent.target.value;
-    this.setState({
-      input: value,
-    });
-  }
+    setInput(value);
+  };
 
-  render() {
-    return (
-      <div>
-        <div id="inputList" className="input-group">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Add ToDo"
-            value={this.state.input}
-            onChange={this.updateInput}
-          />
-          <div className="input-group-prepend">
-            <button className="btn btn-primary" onClick={this.handleAddToDo}>
-              Add
-            </button>
-          </div>
-        </div>
-        <List
-          list={this.state.todoList}
-          onRemoveToDo={this.handleRemoveToDo}
-          onSortByToDoName={this.handleSortToDoName}
-          onSortByToDoDate={this.handleSortToDoDate}
+  return (
+    <div>
+      <div id="inputList" className="input-group">
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Add ToDo"
+          value={input}
+          onChange={updateInput}
         />
+        <div className="input-group-prepend">
+          <button className="btn btn-primary" onClick={handleAddToDo}>
+            Add
+          </button>
+        </div>
       </div>
-    );
-  }
-}
+      <List
+        list={todo}
+        onRemoveToDo={handleRemoveToDo}
+        // onSortByToDoName={handleSortToDoName}
+        // onSortByToDoDate={handleSortToDoDate}
+      />
+    </div>
+  );
+};
+
+export default Notes;
